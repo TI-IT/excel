039a151b425f45713fff1e578481d35c6d6fa23c
TI-IT <go.tiit.ogle@gmail.com>
2023-08-14 17:53:17 +0300

    03_10_Удаление слушателей.mp4

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,25 +1,25 @@
+import {$} from '@core/Dom';
+
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
+    this.$el = $(selector);
     this.components = options.components || [];
   }
 
   getRoot() {
-    const $root = document.createElement('div');
-    $root.classList.add('excel')
-
-    this.components.forEach(Component => {
-      const $el = document.createElement('div')
-      $el.classList.add(Component.className)
+    const $root = $.create('div', 'excel')
+    this.components = this.components.map(Component => {
+      const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
+      return component
     });
-
     return $root;
   }
 
   render() {
     this.$el.append(this.getRoot());
+    this.components.forEach(component => component.init())
   }
 }
